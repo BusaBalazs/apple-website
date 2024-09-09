a04ef140f62c3d94f5e3cd70b6090b5ba52598c7
Busa Balázs <fishbalazs@gmail.com>
2024-09-09 12:36:11 +0200

    Hero section, video highlights section are done.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React from "react";
+//------------------------------------------------
+import { appleImg, bagImg, searchImg } from "../utils";
+import { navLists } from "../constants";
 
+//------------------------------------------------
+//------------------------------------------------
 const Navbar = () => {
   return (
-    <div>Navbar</div>
-  )
-}
+    <header className="w-full p-5 sm:px-10 flex justify-between items-center">
+      <nav className="flex w-full screen-max-width">
+        <img src={appleImg} alt="apple" width={14} height={18} />
+        <ul className="flex flex-1 gap-10 justify-center max-sm:hidden">
+          {navLists.map((navItem) => (
+            <li key={navItem} className="text-sm cursor-pointer text-gray hover:text-white transition-all">{navItem}</li>
+          ))}
+        </ul>
+        <div className="flex items-baseline gap-7 max-sm:justify-end max-sm:flex-1">
+          <img src={searchImg} alt="search" width={18} height={18}/>
+          <img src={bagImg} alt="bag" width={18} height={18}/>
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Navbar
+export default Navbar;
